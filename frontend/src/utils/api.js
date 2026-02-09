9bf2a9bf6b64dd74dd7c038a52c3939e15b1c05b
Rohan Singh <rohansinghiitian6395@gmail.com>
2026-02-10 02:55:21 +0530

    Connected frontend to Railway backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,8 +3,9 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     window.location.hostname === "localhost"
-      ? "http://localhost:3100"
-      : "https://massswapsomailer-m3hql91c0-rohansinghiitian6395-4082s-projects.vercel.app",
+      ? "http://localhost:3100/api"
+      : "https://bulkmailing-production.up.railway.app/api",
+  withCredentials: true,
 });
 
 export default API;
