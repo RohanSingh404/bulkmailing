171c86f40f6eeb2b8d8b93186c7ad9d857cd8cc7
Rohan Singh <rohansinghiitian6395@gmail.com>
2026-02-10 03:26:03 +0530

    Fix double /api issue

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://localhost:3100/api"
-      : "https://bulkmailing-production.up.railway.app/api",
+      : "https://bulkmailing-production.up.railway.app",
   withCredentials: true,
 });
 
