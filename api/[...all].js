dbd9c0efec5804230371de7d33413dc440c44dae
Rohan Singh <rohansinghiitian6395@gmail.com>
2026-02-10 01:50:56 +0530

    saving

--- api/[...all].js
+++ api/[...all].js
@@ -1,14 +1,3 @@
 const app = require("../backend/server");
 
-module.exports = (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://bulkmailingswapsobtc.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  return app(req, res);
-};
+module.exports = app;
